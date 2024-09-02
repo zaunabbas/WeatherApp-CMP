512baef85abf9934e8c08d93c60f47ce9dfcbf93
zaun <s.zain.abbasrizvi@gmail.com>
2024-09-02 13:58:52 +0500

    Remove commented code

--- composeApp/src/commonMain/kotlin/App.kt
+++ composeApp/src/commonMain/kotlin/App.kt
@@ -10,19 +10,4 @@ fun App() {
     WeatherAppTheme{
         Navigator(HomeScreen)
     }
-   /* MaterialTheme {
-        var showContent by remember { mutableStateOf(false) }
-        Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
-            Button(onClick = { showContent = !showContent }) {
-                Text("Click me!")
-            }
-            AnimatedVisibility(showContent) {
-                val greeting = remember { Greeting().greet() }
-                Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
-                    Image(painterResource(Res.drawable.compose_multiplatform), null)
-                    Text("Compose: $greeting")
-                }
-            }
-        }
-    }*/
 }
